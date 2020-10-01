a1ab7c0f062932aef8c8279606848b952becbd9a
Lem0nRavioli <57152284+Lem0nRavioli@users.noreply.github.com>
2020-10-01 22:49:43 +0200

    Add files via upload

---
+++ lazy_bs_downloader.py
@@ -0,0 +1,41 @@
+import requests
+import urllib.request
+import time
+import os
+
+URL_HASH = "https://beatsaver.com/api/maps/by-hash/"
+BS_URL = "http://beatsaver.com"
+headers = {"User-Agent": "Please/Dont/Block"}
+
+os.makedirs("BeatSong")
+
+
+def download_song(song_url, filename):
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('user-agent', 'Please/Dont/Block')]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(song_url, 'BeatSong/' + filename + ".zip")
+
+
+# categories value for the cat variable
+"""
+0 = trending
+1 = date ranked
+2 = scores set
+3 = star rating
+4 = author
+"""
+cat = 0
+limit = 2  # number of song to download
+start = 1  # if already loaded song, define starting point to resume download
+
+# r.json()['songs'][i]['id'] return the song hash to input into the url "https://beatsaver.com/api/maps/by-hash/"
+r = requests.get(f"http://scoresaber.com/api.php?function=get-leaderboards&cat={cat}&page=1&limit={limit}")
+
+for song in r.json()['songs'][start:]:
+    song_hash = song['id']  # retrieve unique song hash
+    direct_dl = requests.get(URL_HASH + song_hash, headers=headers).json()['directDownload']  # retrieve direct dl link
+    url_dl = BS_URL + direct_dl  # add direct dl link to https://beatsaver.com
+    download_song(url_dl, song_hash)  # download the file
+    print(f"Downloading {song['name']} with a rating of {song['stars']}")
+    time.sleep(1)  # pause to not overload website
